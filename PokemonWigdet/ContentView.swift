59a66e9408bc4aeb1410dfd7f02f84ace29d438e
HakanSezer <hakanszer1@gmail.com>
2024-02-02 18:57:14 +0300

    First Widget

---
+++ PokemonWigdet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PokemonWigdet
+//
+//  Created by Hakan Sezer on 2.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image("bullbasaur")
+                .resizable()
+                .frame(width: 100, height: 100)
+                
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PokemonWigdet/PokemonWigdetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonWigdetApp.swift
+//  PokemonWigdet
+//
+//  Created by Hakan Sezer on 2.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct PokemonWigdetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
